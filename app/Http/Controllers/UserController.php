9755b4e34909d495d14c49f57982ce2e8183e6ed
adnan <aturjoy4@gmail.com>
2021-10-09 16:43:50 +0600

    login api check

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,4 +16,14 @@ class UserController extends Controller
        $user->save();
        return $user;
     }
+
+    function login(Request $req){
+        $user= User:: where('email',$req->email)->first();
+        if(!$user || Hash::check($user->password,$req->password)){
+           return response([
+               'error' => ['Email or password is not matched']
+           ]);
+        }
+        return $user;
+    }
 }
